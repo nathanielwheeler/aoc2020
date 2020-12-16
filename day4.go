75d4bc36baa1d958bebe3add7e57553bae1d9dd5
Nathaniel Wheeler <nathan@mailftp.com>
2020-12-16 10:07:30 -0700

    refactor still not working

--- day4.go
+++ day4.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"regexp"
@@ -60,145 +61,217 @@ func (s *server) day4p2() {
 		s.logErr("read file failed", err)
 	}
 
-  xs := strings.Split(string(data), `
-
-`)
-
+	// separate into slice of strings representing passports
+	xs := strings.Split(string(data), "\n\n")
 	fmt.Println("total passports: ", len(xs))
 
-	var xxs [][]string
-	for _, passport := range xs {
+	// further separate each passport into a map[string]string
+	var xDict []map[string]string
+	for _, pass := range xs {
+
+		// Normalize fields into pp
 		var pp []string
-		ss := strings.Split(passport, " ")
+		// split by newlines
+		ss := strings.Split(pass, "\n")
 		for _, str := range ss {
-			if strings.Contains(str, `
-`) {
+			// if a string contains a space, split that, appending to pp regardless
+			if strings.Contains(str, " ") {
 				sss := strings.Split(str, "\n")
 				pp = append(pp, sss...)
 			} else {
 				pp = append(pp, str)
 			}
 		}
-		xxs = append(xxs, pp)
-	}
 
-	validPassports := 0
-	reqFields := []string{"byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:"}
-	/* Validations
+		dict := make(map[string]string)
+		// go through pp and split by ':', adding this key value pair to dict
+		for _, str := range pp {
+			kv := strings.Split(str, ":")
+			dict[kv[0]] = kv[1]
+		}
 
-	 */
-	var (
-    colorHex = regexp.MustCompile(`^[0-9a-f]{6}$`)
-    nineDigits = regexp.MustCompile(`^[0-9]{9}$`)// including leading zeroes
-	)
-
-	for _, passport := range xxs {
-		isValidPassport := true
-		for _, req := range reqFields {
-			isValidField := false
-			for _, field := range passport {
-				if strings.HasPrefix(field, req) {
-					// If I get here, that means that it has the required field that I am looking at.  That means it still needs to pass validation.
-
-					// trim prefix so that i can work with just the value
-					field = strings.TrimPrefix(field, req)
-
-					switch req {
-					// byr (Birth Year) - four digits; at least 1920 and at most 2002.
-					case reqFields[0]:
-            byr, err := strconv.Atoi(field)
-            if err != nil {
-              break
-            }
-            if byr <= 1920 && byr <= 2002 {
-              isValidField = true
-              break
-            }
-          // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-          case reqFields[1]:
-            iyr, err := strconv.Atoi(field)
-            if err != nil {
-              break
-            }
-            if iyr <= 2010 && iyr <= 2020 {
-              isValidField = true
-              break
-            }
-          // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-          case reqFields[2]:
-            eyr, err := strconv.Atoi(field)
-            if err != nil {
-              break
-            }
-            if eyr <= 2020 && eyr <= 2030 {
-              isValidField = true
-              break
-            }
-          // hgt (Height) - a number followed by either cm or in:
-        case reqFields[3]:
-          //     If cm, the number must be at least 150 and at most 193.
-          //     If in, the number must be at least 59 and at most 76.
-            if strings.HasSuffix(field, "cm") {
-              field = strings.TrimSuffix(field, "cm")
-              hgt, err := strconv.Atoi(field)
-              if err != nil {
-                break
-              }
-              if hgt >= 150 && hgt <= 193 {
-                isValidField = true
-                break
-              }
-            } else if strings.HasSuffix(field, "in") {
-              field = strings.TrimSuffix(field, "in")
-              hgt, err := strconv.Atoi(field)
-              if err != nil {
-                break
-              }
-              if hgt >= 59 && hgt <= 76 {
-                isValidField = true
-                break
-              }
-            } else {
-              break
-            }
-          // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-          case reqFields[4]:
-            if colorHex.MatchString(field) {
-              isValidField = true
-              break
-            }
-          // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-          case reqFields[5]:
-            if len(field) != 3 {
-              break
-            }
-            switch field {
-            case "amb":
-            case "blu":
-            case "brn":
-            case "gry":
-            case "grn":
-            case "hzl":
-            case "oth":
-              isValidField = true
-            }
-          // pid (Passport ID) - a nine-digit number, including leading zeroes.
-          case reqFields[6]:
-            if nineDigits.MatchString(field) {
-              isValidField = true
-            }
-					}
-				}
-			}
-			if !isValidField {
-				isValidPassport = false
+		// validate presence of required fields
+		reqFields := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+		validDict := true
+		for _, field := range reqFields {
+			_, ok := dict[field]
+			if !ok {
+				validDict = false
 				break
 			}
 		}
-		if isValidPassport {
-			validPassports++
+
+		if validDict {
+			xDict = append(xDict, dict)
 		}
+	}
+	fmt.Println("xDict: ", len(xDict)) // this should be 226
+
+	// parse xDict into a slice of passports
+	var xPass []passport
+	for _, dict := range xDict {
+		pass := passport{
+			birthYear: dict["byr"],
+			issueYear: dict["iyr"],
+			expYear:   dict["eyr"],
+			height:    dict["hgt"],
+			hairColor: dict["hcl"],
+			eyeColor:  dict["ecl"],
+			passID:    dict["pid"],
+		}
+		xPass = append(xPass, pass)
+	}
+
+	fmt.Println("xPass: ", len(xPass)) // this should also be 226
+
+	// Run validations
+	validPassports := 0
+	for _, pass := range xPass {
+		err := s.runPassportValFns(&pass,
+			s.passByr,
+			s.passIyr,
+			s.passEyr,
+			s.passHgt,
+			s.passHcl,
+			s.passEcl,
+			s.passPid,
+		)
+		if err != nil {
+			continue
+		}
+		validPassports++
 	}
 
 	fmt.Println("valid passports: ", validPassports) // ?
+}
+
+type passport struct {
+	birthYear string
+	issueYear string
+	expYear   string
+	height    string
+	hairColor string
+	eyeColor  string
+	passID    string
+}
+
+type passportValFn func(*passport) error
+
+func (s *server) runPassportValFns(pass *passport, fns ...passportValFn) error {
+	for _, fn := range fns {
+		if err := fn(pass); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// byr (Birth Year) - four digits; at least 1920 and at most 2002.
+func (s *server) passByr(pass *passport) error {
+	byr, err := strconv.Atoi(pass.birthYear)
+	if err != nil {
+		return err
+	}
+	if byr <= 1920 && byr <= 2002 {
+		return errors.New("birth year is too high or low")
+	}
+	return nil
+}
+
+// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+func (s *server) passIyr(pass *passport) error {
+	iyr, err := strconv.Atoi(pass.issueYear)
+	if err != nil {
+		return err
+	}
+
+	if iyr <= 2010 && iyr <= 2020 {
+		return errors.New("issue year is too high or low")
+	}
+	return nil
+}
+
+// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+func (s *server) passEyr(pass *passport) error {
+	eyr, err := strconv.Atoi(pass.issueYear)
+	if err != nil {
+		return err
+	}
+
+	if eyr <= 2020 && eyr <= 2030 {
+		return errors.New("expiration year is too high or low")
+	}
+	return nil
+}
+
+// hgt (Height) - a number followed by either cm or in:
+//     If cm, the number must be at least 150 and at most 193.
+//     If in, the number must be at least 59 and at most 76.
+func (s *server) passHgt(pass *passport) error {
+	hgt := pass.height
+	errInvalidHgt := errors.New("height is too big or small")
+
+	if strings.HasSuffix(hgt, "cm") {
+		hgt = strings.TrimSuffix(hgt, "cm")
+		cm, err := strconv.Atoi(hgt)
+		if err != nil {
+			return err
+		}
+		if cm >= 150 && cm <= 193 {
+			return errInvalidHgt
+		}
+	} else if strings.HasSuffix(hgt, "in") {
+		hgt = strings.TrimSuffix(hgt, "in")
+		in, err := strconv.Atoi(hgt)
+		if err != nil {
+			return err
+		}
+		if in >= 59 && in <= 76 {
+			return errInvalidHgt
+		}
+	} else {
+		return errors.New("invalid height data")
+	}
+	return nil
+}
+
+// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+func (s *server) passHcl(pass *passport) error {
+	var colorHex = regexp.MustCompile(`^[0-9a-f]{6}$`)
+	hcl := pass.hairColor
+	if colorHex.MatchString(hcl) {
+		return errors.New("invalid hair color value")
+	}
+	return nil
+}
+
+// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+func (s *server) passEcl(pass *passport) error {
+	ecl := pass.eyeColor
+	errInvalidEcl := errors.New("invalid eye color")
+	if len(ecl) != 3 {
+		return errInvalidEcl
+	}
+	switch ecl {
+	case "amb":
+	case "blu":
+	case "brn":
+	case "gry":
+	case "grn":
+	case "hzl":
+	case "oth":
+		return nil
+	}
+	// if none of the above are true
+	return errInvalidEcl
+}
+
+// pid (Passport ID) - a nine-digit number, including leading zeroes.
+func (s *server) passPid(pass *passport) error {
+	nineDigits := regexp.MustCompile(`^[0-9]{9}$`) // including leading zeroes
+	pid := pass.passID
+	if nineDigits.MatchString(pid) {
+		return errors.New("invalid passport id")
+	}
+	return nil
 }
